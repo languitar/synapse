2fe1743f3dc9d6fc5d1b32783e2ea7fcf74f4294
Andrew Morgan <andrew@amorgan.xyz>
2021-03-10 17:19:33 +0000

    Fix config module loading

--- synapse/config/server.py
+++ synapse/config/server.py
@@ -245,10 +245,13 @@ class ServerConfig(Config):
             self.use_presence = config.get("use_presence", True)
 
         # Custom presence router module
-        presence_router = presence_config.get("presence_router")
-        self.presence_router_module, self.presence_router_config = load_module(
-            presence_router, ("presence", "presence_router")
-        )
+        self.presence_router_module = None
+        self.presence_router_config = None
+        presence_router_config = presence_config.get("presence_router")
+        if presence_router_config:
+            self.presence_router_module, self.presence_router_config = load_module(
+                presence_router_config, ("presence", "presence_router")
+            )
 
         # Whether to update the user directory or not. This should be set to
         # false only if we are updating the user directory in a worker
